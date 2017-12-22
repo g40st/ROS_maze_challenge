429fa0666b895c115eab5949fe7d54d765291d82
Christian Högerle <ch-171744@hs-weingarten.de>
2017-12-22 14:23:39 +0100

    Closes #2

--- maze/src/maze.py
+++ maze/src/maze.py
@@ -4,35 +4,66 @@ import rospy
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+from pid import PID
 
 class MazeSolver:
 
     def __init__(self):
         rospy.init_node('MazeSolverNode')
-
         self.rate = rospy.Rate(10)
 
+        rospy.Subscriber('/laserscan', LaserScan, self.laserscan_callback)
         rospy.Subscriber('/odom', Odometry, self.odom_callback)
+
         self.velPub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size = 5)
 
         self.vel = Twist()
-        self.odom_x = 0
+        self.laser = None
+        self.odom = None
 
+        # set up wall follower
+        self.distanceToWall = 0.5
 
-    def odom_callback(self, data):
-        self.odom_x = data.pose.pose.position.x
+        # set up PID
+        kp = 6
+        ki = 2
+        kd = 1.5
+        outMin = -0.4
+        outMax = 0.4
+        iMin = -0.4
+        iMax = 0.4
+        self.pid = PID(kp, ki, kd, outMin, outMax, iMin, iMax)
+
+    def odom_callback(self, odom_msg):
+        self.odom = odom_msg
+
+    def laserscan_callback(self, laser_msg):
+        self.laser = laser_msg
 
     def startSolver(self):
         rospy.loginfo("start Maze Solver Node")
 
         while not rospy.is_shutdown():
-            rospy.loginfo(self.odom_x)
 
-            self.vel.linear.x = 0.1
-            self.velPub.publish(self.vel)
+            if(self.laser and self.odom): # laser and odom data received from callback
+                pidValue = self.pid.pidExecute(self.distanceToWall, self.laser.ranges[359])
+                #rospy.loginfo(pidValue)
+                self.wallFollower(pidValue)
+
+                self.velPub.publish(self.vel)
 
             self.rate.sleep()
         rospy.spin()
+
+    def wallFollower(self, pidValue):
+        if(pidValue == 0):
+            self.vel.linear.x = 0.3
+            self.vel.angular.z = 0.0
+        elif(pidValue != 0):
+            self.vel.linear.x = 0.15
+            self.vel.angular.z = pidValue
 
 if __name__ == "__main__":
         maze_solver = MazeSolver()
---
+++ maze/src/pid.py
@@ -0,0 +1,34 @@
+import rospy
+
+class PID:
+    
+    def __init__(self, kp, ki, kd, outMin, outMax, iMin, iMax):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.outMin = outMin
+        self.outMax = outMax
+        self.intMin = iMin
+        self.intMax = iMax
+
+        self.lastError = 0
+        self.sumError = 0
+        self.lastTime = 0
+
+    def pidExecute(self, should, actValue):
+        now = rospy.Time.now().to_sec()
+        timeChange = now - self.lastTime
+        error = should - actValue
+        newErrorSum = self.sumError + (error * timeChange)
+
+        if((newErrorSum >= self.intMin) and (newErrorSum <= self.intMax)):
+            self.sumError = newErrorSum
+        dError = (error - self.lastError) / timeChange
+        output = (self.kp * error) + (self.ki * self.sumError) + (self.kd * dError)
+        self.lastError = error
+        self.last = now
+        if(output > self.outMax):
+            output = self.outMax
+        if(output < self.outMin):
+            output = self.outMin
+        return output*(-1)
